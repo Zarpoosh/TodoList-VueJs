f0755026283ee6dcfb2c68ee4a9425c40cc3e703
Zarpoosh <mzar67839@gmail.com>
2024-08-22 16:50:58 +0330

    add css to addproject

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import AddProject from '@/views/AddProject.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/add',
+      name: 'AddProject',
+      component: AddProject
     }
   ]
 })
